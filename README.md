69ae0504209ab1242ee4fdae59a6256d1b1d92c0
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-08-05 13:15:37 -0600

    created user_colors.py class

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-import random
+PYTHON MASTER MIND.
 
-class SecretColors:
+By Kevin Steven Alpízar Díaz
+
+
+secret_colors.py
+
+la clase secret_colors.py es creada para generar un array con colores secretos dando como parametro, el color y la cantidad de los colores a escoger.
+
+<!-- class SecretColors:
     def __init__(self, colors, length):
         self.colors = colors
         self.length = length
@@ -8,4 +15,4 @@ class SecretColors:
 
     @property
     def get_secret_colors(self):
-        return self.secret_colors
+        return self.secret_colors -->
---
+++ print_colors.py
@@ -0,0 +1,16 @@
+from colorama import Fore
+
+class PrintColors:
+    def print_colors(self, colors):
+        array_with_color = []
+        for color in colors:
+            if color == "red":
+                array_with_color.append(Fore.RED + "red")
+            elif color == "blue":
+                array_with_color.append(Fore.BLUE + "blue")
+            elif color == "green":
+                array_with_color.append(Fore.GREEN + "green")
+            elif color == "yellow":
+                array_with_color.append(Fore.YELLOW + "yellow")
+        return print( array_with_color)  
+print(PrintColors())
