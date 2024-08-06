776bd31d8064d0c100fcec1cd047a8674eaa105c
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-08-06 11:54:39 -0600

    Errors fixed

--- user_colors.py
+++ user_colors.py
@@ -1,37 +1,3 @@
-# from colorama import Fore, Style, init
-
-# init(autoreset=True)
-
-# class UserColors:
-#     def __init__(self, length):
-#         self.length = length
-
-#     def get_colors(self):
-#         user_colors = []
-#         for _ in range(self.length):
-#             # saludo = input("Hola cual es tu nombre:  ")
-#             # print("  saludo = input("Hola cual es tu nombre:  ")")
-#             print(user_colors)
-#             print(f"{Fore.GREEN}[red, green, blue, yellow] decida el orden de colores disponibles:{Style.RESET_ALL}")
-#             color = input().strip().lower()
-#             user_colors.append(color)
-#         return user_colors
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 from colorama import Fore, Style, init
 
 init(autoreset=True)
@@ -45,9 +11,9 @@ class UserColors:
         for _ in range(self.length):
             # saludo = input("Hola cual es tu nombre:  ")
             # print("  saludo = input("Hola cual es tu nombre:  ")")
-            print(user_colors)
-            print(f"{Fore.MAGENTA}[red, green, blue, yellow] decida el orden de colores disponibles:{Style.RESET_ALL}")
+  
+
+            print(f"{Fore.WHITE}[red, green, blue, yellow] Escribe un color a la vez, en orden a :{Style.RESET_ALL}")
             color = input().strip().lower()
             user_colors.append(color)
         return user_colors
-
