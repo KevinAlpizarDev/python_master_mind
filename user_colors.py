eb1677110069cddd023d28616dbbb3b83e97870c
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-08-07 11:26:45 -0600

    Adding ASCII art to the interface

--- user_colors.py
+++ user_colors.py
@@ -2,6 +2,7 @@ from colorama import Fore, Style, init
 
 init(autoreset=True)
 
+
 class UserColors:
     def __init__(self, length):
         self.length = length
@@ -11,9 +12,15 @@ class UserColors:
         for _ in range(self.length):
             # saludo = input("Hola cual es tu nombre:  ")
             # print("  saludo = input("Hola cual es tu nombre:  ")")
-  
-
-            print(f"{Fore.WHITE}[red, green, blue, yellow] Escribe un color a la vez, en orden a :{Style.RESET_ALL}")
+            # print(f"{Fore.WHITE}acontinuacion un ejemplo de una jugada{Style.RESET_ALL}")
+            # print(f"{Fore.WHITE}Ejemplo: {Style.RESET_ALL}")
+            # print(f"{Fore.RED}red + ENTER{Style.RESET_ALL}")
+            # print(f"{Fore.GREEN}green + ENTER{Style.RESET_ALL}")
+            # print(f"{Fore.BLUE}blue + ENTER{Style.RESET_ALL}")
+            # print(f"{Fore.YELLOW}yellow + ENTER{Style.RESET_ALL}")
+            print()
+            print(f"{Fore.WHITE}COMIENZA A JUGAR!{Style.RESET_ALL}")
+            print()
             color = input().strip().lower()
             user_colors.append(color)
         return user_colors
